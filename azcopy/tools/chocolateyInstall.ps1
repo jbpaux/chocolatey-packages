8021f315449b74907ea3b87ff3e3db49bc8a92b4
Jean-Benoit Paux <jean-benoit.paux@avanade.com>
2019-03-13 11:26:17 +0100

    Add azcopy automatic package

---
+++ azcopy/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+ 
+$packageName = 'azcopy'
+ 
+$packageArgs = @{
+    packageName    = $packageName
+    url            = 'https://azcopy.azureedge.net/azcopy-8-1-0/MicrosoftAzureStorageAzCopy_netcore_x64.msi'
+    fileType       = "msi"
+ 
+    softwareName   = 'Microsoft Azure Storage AzCopy*'
+    checksum       = ''
+    checksumType   = ''
+ 
+    silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+    validExitCodes = @(0, 3010, 1641)
+}
+ 
+Install-ChocolateyPackage @packageArgs
---
+++ azcopy/update.ps1
@@ -0,0 +1,29 @@
+import-module au
+
+$releases = 'https://aka.ms/downloadazcopy'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -MaximumRedirection 0 -ErrorAction SilentlyContinue
+
+    $url32 = $download_page.Headers.Location
+    if ( $url32 -match "azcopy-(?<version>[\d-]+)/") {
+        $version = $Matches["version"] -replace '-', '.'
+    }
+
+    @{
+        URL32   = $url32
+        Version = $version
+    }
+}
+
+update -ChecksumFor 32
