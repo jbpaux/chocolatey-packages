e9304b3789ea8168ae002682073309551d40680b
jbpaux <9682558+jbpaux@users.noreply.github.com>
2019-03-13 17:35:32 +0000

    AU: 1 updated - azcopy [skip ci] https://gist.github.com/61135c8432362efde3a4bb0eecf4b604/df031820de0e6b0efcc81732dbb709e4799babc7

--- azcopy/tools/chocolateyInstall.ps1
+++ azcopy/tools/chocolateyInstall.ps1
@@ -8,8 +8,8 @@ $packageArgs = @{
     fileType       = "msi"
  
     softwareName   = 'Microsoft Azure Storage AzCopy*'
-    checksum       = ''
-    checksumType   = ''
+    checksum       = 'a84f70851bac44c4f1d977f510744e394f34471e9bd5655782ea4f7551b893af'
+    checksumType   = 'sha256'
  
     silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
     validExitCodes = @(0, 3010, 1641)
