c009e2a9f9e06cce49c90cb1e3c4072b5db24453
jbpaux <9682558+jbpaux@users.noreply.github.com>
2019-05-20 18:09:45 +0200

    Add azcopy10 package

---
+++ azcopy10/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+ 
+$packageName = 'azcopy10'
+$version = '10.1.2'
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+    packageName    = $packageName
+    url64          = 'https://azcopyvnext.azureedge.net/release20190517/azcopy_windows_amd64_10.1.2.zip'
+    checksum64     = '9efc6744de46d68852f0510e48233095d79049bb9949d7ddb5ca3f1980a7a560'
+    checksumType64 = 'sha256'
+    destination    = $toolsDir
+}
+
+#Manage azcopy installation
+Install-ChocolateyZipPackage @packageArgs
+Rename-Item -Path (Join-Path -Path $toolsDir -ChildPath "azcopy_windows_amd64_$version" ) -NewName 'azcopy'
+
---
+++ azcopy10/update.ps1
@@ -0,0 +1,28 @@
+import-module au
+
+$releases = 'https://aka.ms/downloadazcopy-v10-windows'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^\s*checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+            "(?i)(^\s*\`$version\s*=\s*)('.*')"        = "`$1'$($Latest.Version)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -MaximumRedirection 0 -ErrorAction SilentlyContinue
+
+    $url64 = $download_page.Headers.Location
+    $version = $url64 -replace ".zip", "" -split "_" | Select-Object -Last 1
+
+    @{
+        URL64   = $url64
+        Version = $version
+    }
+}
+
+update -ChecksumFor 64
