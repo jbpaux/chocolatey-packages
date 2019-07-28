8f9ccecb8df1a5d83f6ac3572202a43320eb989b
Jean-Benoit Paux <9682558+jbpaux@users.noreply.github.com>
2019-07-28 19:06:51 +0200

    [PUSH azcopy10] Manage correct update issues

--- azcopy10/tools/chocolateyInstall.ps1
+++ azcopy10/tools/chocolateyInstall.ps1
@@ -13,5 +13,10 @@ $packageArgs = @{
 
 #Manage azcopy installation
 Install-ChocolateyZipPackage @packageArgs
-Rename-Item -Path (Join-Path -Path $toolsDir -ChildPath (($packageArgs.url64 -split '/' | Select-Object -Last 1) -replace ".zip","") ) -NewName 'azcopy'
+
+$targetPath = Join-Path -Path $toolsDir -ChildPath 'azcopy'
+If (Test-Path -Path $targetPath -PathType:Container) {
+    Remove-Item -Path $targetPath -Force
+}
+Rename-Item -Path (Join-Path -Path $toolsDir -ChildPath (($packageArgs.url64 -split '/' | Select-Object -Last 1) -replace ".zip", "") ) -NewName 'azcopy'
 
