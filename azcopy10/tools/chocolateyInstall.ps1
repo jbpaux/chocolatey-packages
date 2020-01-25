3334ae4bba305cbf0d0baec360168567555ef12f
Jean-Benoit Paux <9682558+jbpaux@users.noreply.github.com>
2020-01-25 17:24:17 +0100

    Add -Recurse to azcopy10 install script to avoid message

--- azcopy10/tools/chocolateyInstall.ps1
+++ azcopy10/tools/chocolateyInstall.ps1
@@ -16,7 +16,7 @@ Install-ChocolateyZipPackage @packageArgs
 
 $targetPath = Join-Path -Path $toolsDir -ChildPath 'azcopy'
 If (Test-Path -Path $targetPath -PathType:Container) {
-    Remove-Item -Path $targetPath -Force
+    Remove-Item -Path $targetPath -Force -Recurse
 }
 Rename-Item -Path (Join-Path -Path $toolsDir -ChildPath (($packageArgs.url64 -split '/' | Select-Object -Last 1) -replace ".zip", "") ) -NewName 'azcopy'
 
