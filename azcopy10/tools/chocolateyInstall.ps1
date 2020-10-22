6680805af1d2065152a0e1b6c2cf4cbe05cec4b1
Jean-Benoit Paux <9682558+jbpaux@users.noreply.github.com>
2020-10-22 12:34:13 +0200

    [AU azcopy10]

--- azcopy10/tools/chocolateyInstall.ps1
+++ azcopy10/tools/chocolateyInstall.ps1
@@ -18,5 +18,4 @@ $targetPath = Join-Path -Path $toolsDir -ChildPath 'azcopy'
 If (Test-Path -Path $targetPath -PathType:Container) {
     Remove-Item -Path $targetPath -Force -Recurse
 }
-Rename-Item -Path (Join-Path -Path $toolsDir -ChildPath (($packageArgs.url64 -split '/' | Select-Object -Last 1) -replace ".zip", "") ) -NewName 'azcopy'
-
+Rename-Item -Path (Join-Path -Path $toolsDir -ChildPath (($packageArgs.url64.replace('\', '/') -split '/' | Select-Object -Last 1) -replace ".zip", "") ) -NewName 'azcopy' -Force
