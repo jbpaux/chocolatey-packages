f7e433701f279e9e4ce76ce1d79aada2156807d2
jbpaux <9682558+jbpaux@users.noreply.github.com>
2021-04-15 03:00:59 +0000

    AU: 1 updated - azcopy10 [skip ci] https://gist.github.com/61135c8432362efde3a4bb0eecf4b604

--- azcopy10/tools/chocolateyInstall.ps1
+++ azcopy10/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
     packageName    = $packageName
-    url64          = 'https://azcopyvnext.azureedge.net/release20210226/azcopy_windows_amd64_10.9.0.zip'
-    checksum64     = '562a3bbf731cfead69266b6a779ef0b2b7fa4390b35fbedcbe22c8d67495ec9c'
+    url64          = 'https://azcopyvnext.azureedge.net/release20210415/azcopy_windows_amd64_10.10.0.zip'
+    checksum64     = '4c797ba463ebedf5acdaab4deed40b51fe719167594e2d48cc6ef6fddacaacb7'
     checksumType64 = 'sha256'
     destination    = $toolsDir
 }
