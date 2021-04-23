5e2e5cba46aa855673731fae1f21845cd2cab6df
jbpaux <9682558+jbpaux@users.noreply.github.com>
2021-04-23 17:20:32 +0200

    Add x86 azcopy10 [PUSH azcopy10]

--- azcopy10/tools/chocolateyInstall.ps1
+++ azcopy10/tools/chocolateyInstall.ps1
@@ -8,6 +8,9 @@ $packageArgs = @{
     url64          = 'https://azcopyvnext.azureedge.net/release20210415/azcopy_windows_amd64_10.10.0.zip'
     checksum64     = '4c797ba463ebedf5acdaab4deed40b51fe719167594e2d48cc6ef6fddacaacb7'
     checksumType64 = 'sha256'
+    url            = 'https://azcopyvnext.azureedge.net/release20210415/azcopy_windows_386_10.10.0.zip'
+    checksum       = 'b40e0f800c18487f708cf1c0a9c3a7621428f2507f82c1ff80054b5aefe8e40a'
+    checksumType   = 'sha256'
     destination    = $toolsDir
 }
 
