0d767844f99de76e1c22019a396a567a1bf63c17
jbpaux <9682558+jbpaux@users.noreply.github.com>
2021-04-26 11:08:42 +0200

    Fix install script for x86 version [PUSH azcopy10]

--- azcopy10/tools/chocolateyInstall.ps1
+++ azcopy10/tools/chocolateyInstall.ps1
@@ -21,4 +21,5 @@ $targetPath = Join-Path -Path $toolsDir -ChildPath 'azcopy'
 If (Test-Path -Path $targetPath -PathType:Container) {
     Remove-Item -Path $targetPath -Force -Recurse
 }
-Rename-Item -Path (Join-Path -Path $toolsDir -ChildPath (($packageArgs.url64.replace('\', '/') -split '/' | Select-Object -Last 1) -replace ".zip", "") ) -NewName 'azcopy' -Force
+
+Get-ChildItem -Path $toolsDir -Directory -Filter "azcopy*" | Rename-Item  -NewName 'azcopy' -Force
