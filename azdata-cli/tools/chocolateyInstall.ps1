eda47b68e886a6013c6b3ce54bae66a0f8757a66
jbpaux <9682558+jbpaux@users.noreply.github.com>
2022-06-15 03:01:35 +0000

    AU: 1 updated - azdata-cli [skip ci] https://gist.github.com/61135c8432362efde3a4bb0eecf4b604/43834e45165f0a681b7d1f603910f2682be9bc5a

--- azdata-cli/tools/chocolateyInstall.ps1
+++ azdata-cli/tools/chocolateyInstall.ps1
@@ -4,11 +4,11 @@ $packageName = 'azdata-cli'
  
 $packageArgs = @{
     packageName    = $packageName
-    url            = 'https://download.microsoft.com/download/f/f/f/fffaa914-d4f7-4885-89c7-696bbfe7670a/azdata-cli-20.3.11.msi'
+    url            = 'https://download.microsoft.com/download/f/f/f/fffaa914-d4f7-4885-89c7-696bbfe7670a/azdata-cli-20.3.12.msi'
     fileType       = "msi"
  
     softwareName   = 'Microsoft Data CLI'
-    checksum       = 'e7084b857db856ef99c08f73c77073021d41168d49b4974dfc4499b1dab02908'
+    checksum       = '98b2672002ec154966fd0d9751da92092e003f6d4a79f41eb176fd7529b3ccf0'
     checksumType   = 'sha256'
  
     silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
