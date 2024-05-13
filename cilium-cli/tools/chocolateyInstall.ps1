393968710bb6e6e94aee5e8174d1a58706d596d4
jbpaux <9682558+jbpaux@users.noreply.github.com>
2024-05-13 17:08:12 +0200

    [PUSH cilium-cli] add cilium-cli package

---
+++ cilium-cli/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+ 
+$packageName = 'cilium-cli'
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+    packageName    = $packageName
+    url64          = 'https://github.com/cilium/cilium-cli/releases/download/v0.16.7/cilium-windows-amd64.zip'
+    checksum64     = 'd00778ab5ee21ea85cac62949938320c87dcbd3ab72e7fa00421ca651ed10af1'
+    checksumType64 = 'sha256'
+    destination    = $toolsDir
+}
+
+$targetPath = Join-Path -Path $toolsDir -ChildPath "cilium.exe"
+if (Test-Path $targetPath) {
+    Remove-Item $targetPath
+}
+Install-ChocolateyZipPackage @packageArgs
---
+++ cilium-cli/update.ps1
@@ -0,0 +1,42 @@
+import-module au
+
+$repo = 'cilium/cilium-cli'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*[$]packageName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)'"
+            "(?i)(^\s*url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^\s*checksumType64\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType64)'"
+        }
+
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+
+    }
+}
+
+function global:au_BeforeUpdate { 
+    Remove-Item "$PSScriptRoot\tools\*.exe" -Force
+}
+function global:au_AfterUpdate {  }
+
+function global:au_GetLatest {
+    $releases = Invoke-RestMethod "https://api.github.com/repos/$repo/releases"
+    if ($null -ne $releases) {
+        $release = $releases | Sort-Object published_at -Descending | Select-Object -First 1
+        if ($null -ne $release) {
+            $url64 = ($release.assets | Where-Object name -like "cilium-windows-amd64.zip").browser_download_url
+            $version = $release.tag_name -replace "v", ""
+        
+            @{
+                URL64   = $url64
+                Version = $version
+            }
+        }
+    }
+}
+
+update -ChecksumFor 64
