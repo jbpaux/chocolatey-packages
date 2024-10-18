96f1673497a3e41f1df32b44dc44ab11740b97ee
jbpaux <9682558+jbpaux@users.noreply.github.com>
2024-10-18 18:39:49 +0200

    [PUSH msgraph-cli] Add msgraph cli tool

---
+++ msgraph-cli/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+ 
+$packageName = 'msgraph-cli'
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+    packageName    = $packageName
+    url64          = 'https://github.com/microsoftgraph/msgraph-cli/releases/download/v1.9.0/msgraph-cli-win-x64-1.9.0.zip'
+    checksum64     = '2da778a6f35820fa565399081cea5cc194568a9eeb9527449d43dd050afcbaae'
+    checksumType64 = 'sha256'
+    destination    = $toolsDir
+}
+
+$targetPath = Join-Path -Path $toolsDir -ChildPath "mgc.*"
+if (Test-Path $targetPath) {
+    Remove-Item $targetPath
+}
+Install-ChocolateyZipPackage @packageArgs
