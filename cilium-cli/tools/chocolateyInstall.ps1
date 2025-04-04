3a1eac454ffff3ac86fc61d374cbb1ba31d4c05c
jbpaux <9682558+jbpaux@users.noreply.github.com>
2025-04-04 03:01:09 +0000

    AU: 1 updated - cilium-cli [skip ci] https://gist.github.com/jbpaux/61135c8432362efde3a4bb0eecf4b604/3509a8100859194c6d255893fbb94a9cc6350123

--- cilium-cli/tools/chocolateyInstall.ps1
+++ cilium-cli/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
     packageName    = $packageName
-    url64          = 'https://github.com/cilium/cilium-cli/releases/download/v0.18.2/cilium-windows-amd64.zip'
-    checksum64     = 'baa651d5902152afd41a965dcb1ab9c3f42c03fe709d310e48045a0d1a2aebbc'
+    url64          = 'https://github.com/cilium/cilium-cli/releases/download/v0.18.3/cilium-windows-amd64.zip'
+    checksum64     = '3dd6173fc879a7494793a7a94724dfaff572b3baa2edb971fdd34b1e0c1e98ac'
     checksumType64 = 'sha256'
     destination    = $toolsDir
 }
