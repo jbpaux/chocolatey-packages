94c27fa3f49a9b033335edd2ad7dd9caef031411
jbpaux <9682558+jbpaux@users.noreply.github.com>
2026-08-20 03:01:16 +0000

    AU: 1 updated - bosh-cli [skip ci] https://gist.github.com/jbpaux/61135c8432362efde3a4bb0eecf4b604/634e61cb1fc73f7e7b95a81c7e1ebf288014ffa0

--- bosh-cli/tools/chocolateyInstall.ps1
+++ bosh-cli/tools/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ $toolsDir      = Split-Path $MyInvocation.MyCommand.Definition
 $packageArgs = @{
     PackageName  = $packageName
     FileFullPath = "$toolsDir\$exeName"
-    Url64          = 'https://github.com/cloudfoundry/bosh-cli/releases/download/v7.10.9/bosh-cli-7.10.9-windows-amd64.exe'
-    Checksum64     = 'c0a3596bfe3df74d1bcb949ccb2120990325b4508798d5a8bb2cbb6e873894e4'
+    Url64          = 'https://github.com/cloudfoundry/bosh-cli/releases/download/v7.10.10/bosh-cli-7.10.10-windows-amd64.exe'
+    Checksum64     = 'da1449de1f9601dfc9bcb2b122ea4a99ef269e151fa88400664f3cf27cb73de2'
     ChecksumType = 'sha256'
 }
 Get-ChocolateyWebFile @packageArgs
